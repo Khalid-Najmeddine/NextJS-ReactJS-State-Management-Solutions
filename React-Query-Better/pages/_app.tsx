a7581f18fce28f3493f758f7691e226100fdaa59
Khalid Najmeddine <knajm574@gmail.com>
2022-12-27 15:01:28 -0700

    feat: Solution using React-Query-Better

---
+++ React-Query-Better/pages/_app.tsx
@@ -0,0 +1,15 @@
+import {Hydrate, QueryClient, QueryClientProvider} from "react-query"
+import '../styles/globals.css'
+import React from "react"
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
+}
